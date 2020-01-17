226de72b89c7821af4ed086aed6dcdeddb09762b
dhilipsiva <dhilipsiva@pm.me>
2020-01-17 21:00:59 +0530

    Implement SSR

--- assets/2020-01-18/zoogle/zoogle/contribs/views.py
+++ assets/2020-01-18/zoogle/zoogle/contribs/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
+from zoogle.zmail.models import Zmail
+from zoogle.zdocs.models import Zdoc
+
 
 def index(request):
     return render(request, 'index.html')
 
 
 def ssr(request):
-    return render(request, 'ssr.html')
+    zmails = Zmail.objects.all()
+    zdocs = Zdoc.objects.all()
+    ctx = dict(zmails=zmails, zdocs=zdocs)
+    return render(request, 'ssr.html', ctx)
 
 
 def ajax(request):
